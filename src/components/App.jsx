d6805f87b48911fe45096e131903da9f0f045e33
TheTempest7 <galaxy.yegor.uaz.s4@gmail.com>
2023-06-08 20:12:06 +0300

    hot fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import Navbarbody from "./Navbarbody";
 import AppRouter from "./AppRouter";
-
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
 
   return (
-    <div>
+    <BrowserRouter>
       <Navbarbody/>
       <AppRouter/>
-    </div>
+    </BrowserRouter>
   )
 }
 
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes ,Navigate,BrowserRouter} from "react-router-dom";
+import { Route, Routes ,Navigate} from "react-router-dom";
 import PostPagination from "./Posts/PostPagination";
 import UserSpecifics from "./UserSpecifics";
 const AboutMe = React.lazy(()=>import("./AboutMe"));
@@ -9,17 +9,16 @@ const AboutMe = React.lazy(()=>import("./AboutMe"));
 
 
 
+
 function AppRouter() {
 
 return (
-    <BrowserRouter>
         <Routes  >
             <Route path='/PostList' element={ <PostPagination/> } />
-            <Route path='/UserSpecifics'  element={<UserSpecifics />} />
-            <Route path='/AboutMe'  element={<AboutMe />} />
-            <Route path="*"  element={<Navigate to="/PostList"/>}/>
+            <Route path='/UserSpecifics'  element={ <UserSpecifics/> } />
+            <Route path='/AboutMe'  element={ <AboutMe/> } />
+            <Route path="*"  element={ <Navigate to="/PostList"/> }/>
         </Routes> 
-    </BrowserRouter>
 )
 }
 
